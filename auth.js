d8085016719dff3519396c8abacf040ca035d1ab
Balbir Prasad <prasadbalbir1056@gmail.com>
2021-03-27 23:11:45 +0530

    Update auth.js
    commented code of auth.js file

--- auth.js
+++ auth.js
@@ -1,11 +1,11 @@
 //signup
-const signupForm=document.querySelector("#signup-form");
-signupForm=addEventListener("click",function (e) {
- e.preventDefault();  
+// const signupForm=document.querySelector("#signup-form");
+// signupForm=addEventListener("click",function (e) {
+//  e.preventDefault();  
 
- //get user info 
- const email=signupForm['signupInputEmail1'].value;
- const password=signupForm['signupInputPassword1'].value;
- const Cpassword=signupForm['signupInputCPassword1'].value;
- console.log(email,password,Cpassword);
-})
+//  //get user info 
+//  const email=signupForm['signupInputEmail1'].value;
+//  const password=signupForm['signupInputPassword1'].value;
+//  const Cpassword=signupForm['signupInputCPassword1'].value;
+//  console.log(email,password,Cpassword);
+// })
